d4cb23d079c81002ea7ef63f03b6aeed4e38b5ed
Ray Zane <ray@promptworks.com>
2018-01-12 12:03:39 -0500

    Add sans_password

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule MyApp.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:sans_password, "~> 1.0.0"},
       {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
